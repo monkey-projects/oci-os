27c39c9f9e5787bdfec3a2a6604fd7e0a7318efe
Wout Neirynck <wout@monkey-projects.be>
2023-09-04 16:34:54 +0200

    Rename/copy calls

--- src/monkey/oci/os/core.clj
+++ src/monkey/oci/os/core.clj
@@ -2,10 +2,12 @@
   (:require [camel-snake-kebab.core :as csk]
             [clojure.data.json :as json]
             [clojure.string :as cs]
+            [clojure.tools.logging :as log]
             [manifold.deferred :as md]
             [martian
              [core :as martian]
-             [httpkit :as martian-http]]
+             [httpkit :as martian-http]
+             [interceptors :as mi]]
             [monkey.oci.sign :as sign]
             [schema.core :as s]))
 
@@ -31,11 +33,19 @@
 
 (defn- sign-request
   "Adds authorization signature to the Martian request"
-  [conf {:keys [request] :as ctx}]
-  (let [sign-headers (-> request
-                         (assoc :url (url-with-query request))
-                         (sign/sign-headers))
+  [conf {:keys [request handler] :as ctx}]
+  (log/info "Signing request with body:" (:body request) "(length:" (count (:body request)) ")")
+  (log/info "Url:" (:url request))
+  (let [sign-headers (cond->
+                         (-> request
+                             (assoc :url (url-with-query request))
+                             (sign/sign-headers))
+                       ;; Special treatment for some put operations
+                         (and (= :put (:method handler))
+                              (= :put-object (:route-name handler)))
+                         (select-keys ["date" "(request-target)" "host"]))
         headers (sign/sign conf sign-headers)]
+    (log/info "Sign headers:" sign-headers)
     (update-in ctx [:request :headers] merge headers)))
 
 (defn signer [conf]
@@ -45,7 +55,15 @@
 (def body-parser
   {:name ::parse-body
    :leave (fn [ctx]
-            (update ctx :response (comp parse-body :body)))})
+            (cond-> ctx
+              (= "application/json" (get-in ctx [:response :headers :content-type]))
+              (update :response (comp parse-body :body))))})
+
+(def bucket-path ["/n/" :ns "/b/" :bucketName])
+(def bucket-path-schema {:ns s/Str :bucketName s/Str})
+
+(def object-path (concat bucket-path ["/o/" :objectName]))
+(def object-path-schema (assoc bucket-path-schema :objectName s/Str))
 
 (def routes
   [{:route-name :get-namespace
@@ -60,13 +78,69 @@
    
    {:route-name :get-bucket
     :method :get
-    :path-parts ["/n/" :ns "/b/" :bucketName]
-    :path-schema {:ns s/Str :bucketName s/Str}}
+    :path-parts bucket-path
+    :path-schema bucket-path-schema}
 
    {:route-name :list-objects
     :method :get
-    :path-parts ["/n/" :ns "/b/" :bucketName "/o"]
-    :path-schema {:ns s/Str :bucketName s/Str}}])
+    :path-parts (conj bucket-path "/o")
+    :path-schema bucket-path-schema
+    :query-schema {(s/optional-key :prefix) s/Str
+                   (s/optional-key :start) s/Str
+                   (s/optional-key :end) s/Str
+                   (s/optional-key :limit) s/Int
+                   (s/optional-key :delimiter) s/Str
+                   (s/optional-key :fields) s/Str
+                   (s/optional-key :startAfter) s/Str}}
+
+   {:route-name :put-object
+    :method :put
+    :path-parts object-path
+    :path-schema object-path-schema
+    :body-schema {:contents s/Any}}
+
+   {:route-name :get-object
+    :method :get
+    :path-parts object-path
+    :path-schema object-path-schema}
+   
+   {:route-name :delete-object
+    :method :delete
+    :path-parts ["/n/" :ns "/b/" :bucketName "/o/" :objectName]
+    :path-schema {:ns s/Str :bucketName s/Str :objectName s/Str}}
+
+   {:route-name :head-object
+    :method :head
+    :path-parts ["/n/" :ns "/b/" :bucketName "/o/" :objectName]
+    :path-schema {:ns s/Str :bucketName s/Str :objectName s/Str}}
+
+   {:route-name :rename-object
+    :method :post
+    :path-parts (conj bucket-path "/actions/renameObject")
+    :path-schema bucket-path-schema
+    :body-schema {:rename {:sourceName s/Str
+                           :newName s/Str
+                           (s/optional-key :srcObjIfMatchETag) s/Str
+                           (s/optional-key :newObjIfMatchETag) s/Str
+                           (s/optional-key :newObjIfNoneMatchETag) s/Str}}
+    :consumes #{"application/json"}}
+
+   {:route-name :copy-object
+    :method :post
+    :path-parts (conj bucket-path "/actions/copyObject")
+    :path-schema bucket-path-schema
+    :body-schema {:copy {:sourceObjectName s/Str
+                         (s/optional-key :sourceObjIfMatchETag) s/Str
+                         (s/optional-key :sourceVersionId) s/Str
+                         :destinationBucket s/Str
+                         :destinationNamespace s/Str
+                         :destinationObjectName s/Str
+                         :destinationRegion s/Str
+                         (s/optional-key :destinationObjectIfMatchETag) s/Str
+                         (s/optional-key :destinationObjectIfNoneMatchETag) s/Str
+                         (s/optional-key :destinationObjectMetadata) s/Any
+                         (s/optional-key :destinationObjectStorageTier) s/Str}}
+    :consumes #{"application/json"}}])
 
 (defn make-context
   "Creates Martian context for the given configuration.  This context
@@ -77,7 +151,8 @@
    routes
    {:interceptors (concat [body-parser]
                           martian/default-interceptors
-                          [(signer conf)
+                          [mi/default-encode-body
+                           (signer conf)
                            martian-http/perform-request])}))
 
 (defn- make-request-fn
@@ -87,7 +162,8 @@
    route definition."
   [id]
   (fn [ctx & [params]]
-    (martian/response-for ctx id params)))
+    (martian/response-for ctx id (merge {::martian/request {:headers {"Accept" "application/json"}}}
+                                        params))))
 
 (def get-namespace (make-request-fn :get-namespace))
 
@@ -95,3 +171,9 @@
 (def get-bucket (make-request-fn :get-bucket))
 
 (def list-objects (make-request-fn :list-objects))
+(def put-object (make-request-fn :put-object))
+(def get-object (make-request-fn :get-object))
+(def delete-object (make-request-fn :delete-object))
+(def head-object (make-request-fn :head-object))
+(def copy-object (make-request-fn :copy-object))
+(def rename-object (make-request-fn :rename-object))
